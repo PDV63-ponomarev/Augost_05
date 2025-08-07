0b0ccfd3f1a4bd3f0a22548b0ddd2cf710ac97e0
My <graiwers@gmail.com>
2025-08-07 12:45:19 +0400

    python

---
+++ python/les3.py
@@ -0,0 +1,19 @@
+# emp = ''
+# print(emp)
+
+# a = 42
+# a = 'text'
+# b = 'NO'
+# c = '3'
+# print(a + ' ' + b + ' ' + c)
+
+# print('e' in 'hello')
+# print('a' in 'hello')
+
+# q1 = 'text'
+# d1 = q1
+# w1 = 'text'
+# print(d1 is q1)
+# print(w1 is d1)
+
+
