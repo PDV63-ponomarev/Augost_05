d058c95a2e5d56a163ae034025f2e089f4f1886b
My <graiwers@gmail.com>
2025-08-18 13:53:24 +0400

    16

---
+++ python/less16/secr.py
@@ -0,0 +1,15 @@
+# import mysql.connector as mysql
+# from /project/Augost_05/python/less16 import creds
+#
+# db = mysql.connect(
+#     user = credits.user,
+#     passwd = credits.passwd,
+#     host = credits.host,
+#     port = credits.port,
+#     database = credits.database
+# )
+#
+# cursor = db.cursor(dictionary=True)
+# cursor.execute('SELECT * FROM students')
+# data = cursor.fetchall()
+# print(data)
