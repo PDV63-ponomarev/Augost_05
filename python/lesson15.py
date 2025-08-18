e9a97ed3295e38d13af606c1e9e00e98406e7727
My <graiwers@gmail.com>
2025-08-18 10:49:53 +0400

    les15

---
+++ python/lesson15.py
@@ -0,0 +1,56 @@
+# подбор чисел
+
+# my_num = 13
+
+# while True:
+#     user_num = int(input('your num: '))
+#     if user_num == my_num:
+#         break
+#     elif user_num > my_num:
+#         print('Число должно быть меньше')
+#     else:
+#         print('Число должно быть больше')
+#
+# print('Верно!')
+
+
+########
+
+
+# my_num = 13
+#
+# while int(input('Your num: ')) != my_num:
+#     print('Try again')
+#
+# print('Congrats!')
+
+
+########
+
+
+# my_num = 13
+#
+# while (user_num := int(input('Your num: '))) != my_num:
+#     if user_num > my_num:
+#         print('Число должно быть меньше')
+#     else:
+#         print('Число должно быть больше')
+#
+# print('Верно!')
+
+
+########
+
+
+# while True:
+#     user_input = input('Say something: ')
+#     match user_input:
+#         case 'Hello':
+#             print('Hi!')
+#         case 'Bye':
+#             print('Good bye!')
+#         case '.':
+#             print('The end')
+#             break
+#         case _: #все иные варианты, аналог else
+#             print('Hello or Bye?')
